9844a1ec7374b992eb9522823a708364f14f926e
mengfanwu <mengfw_em@163.com>
2017-12-19 22:50:47 +0800

    modify

--- src/main/java/stairs/Main.java
+++ src/main/java/stairs/Main.java
@@ -1,26 +1,32 @@
 package stairs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by mengfw on 2017/12/19.
  * N阶楼梯，每次可以走一阶或两阶，从底下走到顶有多少种方案
  */
 public class Main {
-
-    public int getKinds(int step){
+    Map<Integer,Long> map = new HashMap<>();
+    public long getKinds(int step){
+        if(map.containsKey(step)){
+            return map.get(step);
+        }
         if(step == 1){
             return 1;
         }
         if(step == 2){
             return 2;
         }
-        int pre = getKinds(--step);
-        int prePre = getKinds(--step);
-        return pre + prePre;
+        long result = getKinds(--step) + getKinds(--step);
+        map.put(step,result);
+        return result;
     }
 
     public static void main(String[] args) {
         Main main = new Main();
-        int kinds = main.getKinds(4);
+        long kinds = main.getKinds(100);
         System.out.println(kinds);
     }
 }
